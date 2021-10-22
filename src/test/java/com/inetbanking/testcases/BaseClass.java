a0bba2c0836d24f66f2e2d06efa62a1accebfc4d
Academy <pranittiwari1234@gmail.com>
2021-10-22 13:41:45 +0530

    New

--- src/test/java/com/inetbanking/testcases/BaseClass.java
+++ src/test/java/com/inetbanking/testcases/BaseClass.java
@@ -1,26 +1,32 @@
 package com.inetbanking.testcases;
 
 import org.apache.log4j.PropertyConfigurator;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.log4testng.Logger;
 
-import java.util.logging.Logger;
+
+import java.io.IOException;
+
+
 
 public class BaseClass {
     public String baseurl = "http://demo.guru99.com/V1/index.php";
     public String username = "mngr360344";
     public String password = "zApurYt";
     public static WebDriver driver;
-    public static Logger logger;
+    //public static Logger logger;
     @BeforeClass
-    public void setup() {
+    public void setup() throws IOException {
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
         driver = new ChromeDriver();
 
-        Logger logger = Logger.getLogger("ebanking");
-        PropertyConfigurator.configure("Log4j.properties");
+
+   // Logger logger = Logger.getLogger(BaseClass.class);
+      //  PropertyConfigurator.configure("Log4j.properties");
     }
 @AfterClass
     public void teardown()
--- src/test/java/com/inetbanking/testcases/TC_LoginTest_001.java
+++ src/test/java/com/inetbanking/testcases/TC_LoginTest_001.java
@@ -6,25 +6,22 @@ import org.testng.annotations.Test;
 
 public class TC_LoginTest_001 extends com.inetbanking.testcases.BaseClass {
 @Test
-    public void loginTest()
-    {
-driver.get(baseurl);
+    public void loginTest() {
+    driver.get(baseurl);
 
-logger.info("url is opened");
-        LoginPage  lp= new LoginPage(driver);
-        lp.setUsername(username);
-        logger.info("Entered username");
-        lp.setPassword(password);
-        logger.info("Entered password");
-        lp.Clicksubmit();
-        if (driver.getTitle().equals("GTPL Bank Manager HomePage"))
-        {
-            Assert.assertTrue(true);
-            logger.info("Login Test Passed");
-        }
-        else
-        {Assert.assertTrue(false);
-            logger.info("Login Test Failed");}
+//logger.info("url is opened");
+    LoginPage lp = new LoginPage(driver);
+    lp.setUsername(username);
+  //  logger.info("Entered username");
+    lp.setPassword(password);
+    // logger.info("Entered password");
+    lp.Clicksubmit();
+    if (driver.getTitle().equals("GTPL Bank Manager HomePage")) {
+        Assert.assertTrue(true);
+      //   logger.info("Login Test Passed");
+    } else {
+        Assert.assertTrue(false);
+        //logger.info("Login Test Failed");}
 
     }
-}
+}}
